e1d089c5c192f59d40625988e609e24a7db4af59
Kaan <144000125+KaanKalmi@users.noreply.github.com>
2024-09-12 11:06:13 +0200

    all mediaqueries are now working
    fixed the card on 'xs' 's' 'm' or larger screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,198 @@
     import {breakpoint, platform} from '../mediaquery.svelte';
 </script>
 
+{#if $breakpoint == "xs"}
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: rgb(31, 31, 31);
+        color: rgb(145, 213, 240);
+    }
+
+    .businessCard{
+        position: relative;
+        width: 90%;
+        height: 300px;
+        background-color: #252525;
+        border-radius: 8px;
+        border: 2px solid #91d5f025;
+        display: flex;
+        justify-content: center;
+        margin: auto;
+        box-shadow: 4px 8px 12px 8px rgba(0,0,0,0.35);
+
+        & .leftAligned{
+            position: absolute;
+            left: 0;
+            height: 100%;
+            width: 50%;
+            
+            & img{
+                height: 100%;
+                width: 100%;
+                border-radius: 8px 0 0 8px; 
+                border-right: 1px solid #42C9FF;
+            }
+        }
+
+        & .figure{
+            Display: none;
+        }
+        & .rightAligned{
+            margin-left: 1em;
+
+        & h1{
+            position: absolute;
+            top: 1em;
+            margin: .2em;
+            color: #42C9FF;
+            font-size: 1em;
+        }
+
+        & h2{
+            position: absolute;
+            top: 3em;
+            margin: .2em;
+            font-size: 1em;
+            color: #91D5F0;
+        }
+
+        & p {
+            position: absolute;
+            top: 5.5em;
+            margin: .2em;
+            color: #FFFFFF;
+        }
+
+        & .cardContent{
+            position: absolute;
+            bottom: 0;
+
+            & h3{
+                text-align: center;
+                color: #FFFFFF;
+            }
+
+            & ul{
+                list-style-type: none;
+                padding: 0;
+                line-height: 1.6em;
+                display: flex;
+                flex-direction: row;
+
+                & li{
+                    margin: 0 1em;
+
+                & a{
+                    text-decoration: none;
+                    color: black;
+                }
+            }
+            }
+        }
+    }
+    }
+</style>
+{/if}  
+{#if $breakpoint == "s"}
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: rgb(31, 31, 31);
+        color: rgb(145, 213, 240);
+    }
+
+    .businessCard{
+        position: relative;
+        width: 90%;
+        height: 300px;
+        background-color: #252525;
+        border-radius: 8px;
+        border: 2px solid #91d5f025;
+        display: flex;
+        justify-content: center;
+        margin: auto;
+        box-shadow: 4px 8px 12px 8px rgba(0,0,0,0.35);
+
+        & .leftAligned{
+            position: absolute;
+            left: 0;
+            height: 100%;
+            width: 45%;
+            
+            & img{
+                height: 100%;
+                width: 100%;
+                border-radius: 8px 0 0 8px; 
+                border-right: 1px solid #42C9FF;
+            }
+        }
+
+        & .figure{
+            Display: none;
+        }
+        & .rightAligned{
+            margin-left: 1em;
+
+        & h1{
+            position: absolute;
+            top: 1em;
+            margin: .2em;
+            color: #42C9FF;
+        }
+
+        & h2{
+            position: absolute;
+            top: 3em;
+            margin: .2em;
+            font-size: 1.2em;
+            color: #91D5F0;
+        }
+
+        & p {
+            position: absolute;
+            top: 5.5em;
+            margin: .2em;
+            color: #FFFFFF;
+        }
+
+        & .cardContent{
+            position: absolute;
+            bottom: 0;
+
+            & h3{
+                text-align: center;
+                color: #FFFFFF;
+            }
+
+            & ul{
+                list-style-type: none;
+                padding: 0;
+                line-height: 1.6em;
+                display: flex;
+                flex-direction: row;
+
+                & li{
+                    margin: 0 1em;
+
+                & a{
+                    text-decoration: none;
+                    color: black;
+                }
+            }
+            }
+        }
+    }
+    }
+</style>
+{/if}  
+
 {#if $breakpoint == "m"}
 <style>
     .container{
